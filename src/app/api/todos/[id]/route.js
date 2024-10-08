14285fc1a4af21e99c57b61d6be213d07ab1f1d1
ankitjhagithub21 <ajha45075@gmail.com>
2024-10-08 22:09:03 +0530

    added delete todo logic

---
+++ src/app/api/todos/[id]/route.js
@@ -0,0 +1,40 @@
+import dbConnect from '@/lib/db';
+import { verifyToken } from '@/lib/verifyToken';
+import Todo from '@/models/Todo';
+import { NextResponse } from 'next/server';
+
+dbConnect();
+
+export async function DELETE(req,{params}) {
+  // Verify the token from the request
+  const { valid, decoded, error } = verifyToken(req);
+
+  if (!valid) {
+    return NextResponse.json({ success: false, message: error }, { status: 401 });
+  }
+
+  // Get the ID from the request URL
+  const {id} = params;
+  
+
+  try {
+    const todo = await Todo.findById(id);
+
+    if (!todo) {
+      return NextResponse.json({ success: false, message: "Todo not found." }, { status: 404 });
+    }
+
+    // Check if the user is authorized to delete the todo
+    if (todo.userId.toString() !== decoded.id) {
+      return NextResponse.json({ success: false, message: "You cannot delete this todo." }, { status: 403 });
+    }
+
+    // Delete the todo
+    await Todo.deleteOne({ _id: todo._id });
+
+    return NextResponse.json({ success: true, message: "Todo deleted." }, { status: 200 });
+    
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error.message }, { status: 500 });
+  }
+}
